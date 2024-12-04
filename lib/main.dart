bf6c014d106596ecefd87389a12b893e63770621
BasharMOhamed <bashar.eg6645@gmail.com>
2024-12-04 23:37:50 +0200

    Making product details page with dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/productDetails.dart';
 import 'package:flutter_app/firebase_options.dart';
 import 'signUp.dart';
 import 'login.dart';
+
+import 'package:flutter_app/firebase_options.dart';
+import 'package:flutter_app/productDetails.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,10 +21,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Firebase Sign Up',
       home: LoginPage(),
+=======
+    return const MaterialApp(
+      home: Productdetails(),
+>>>>>>> 4489263 (Making product details page with dummy data)
     );
   }
 }
