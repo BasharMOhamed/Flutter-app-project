44892636fc587c3c44854717e742612781f2c83c
BasharMOhamed <bashar.eg6645@gmail.com>
2024-12-04 23:37:50 +0200

    Making product details page with dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/productDetails.dart';
 import 'package:flutter_app/firebase_options.dart';
+
+import 'package:flutter_app/firebase_options.dart';
+import 'package:flutter_app/productDetails.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,11 +20,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Productdetails(),
     );
   }
 }
