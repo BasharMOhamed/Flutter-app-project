f25a21ff927e9562ce641d14f8128110f387afe7
sohilametwaly <sohailarafat934@yahoo.com>
2024-12-07 23:20:42 +0200

    No change

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/firebase_options.dart';
+import 'package:flutter_app/shoppingCart/product-list.dart';
 import 'signUp.dart';
 import 'login.dart';
-import './Product.dart';
-import './shoppingCart/product-list.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(const MainApp());
 }
 
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Firebase Sign Up',
-//       home: LoginPage(),
-//     );
-//   }
-
-// }
-
-class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Shopping Cart',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'Firebase Sign Up',
       home: productList(),
     );
   }
