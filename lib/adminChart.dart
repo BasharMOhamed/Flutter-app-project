bdd6657aa1afb78495a58061bc74b78858445eaf
sohilametwaly <sohailarafat934@yahoo.com>
2024-12-11 14:54:58 +0200

    create chart page

---
+++ lib/adminChart.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+
+class AdminChart extends StatelessWidget {
+  const AdminChart({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title:const Text("Best Sold Products"),),
+    body:const Padding(padding: EdgeInsets.all(10),
+    child: ,
+    )
+    );
+  }
+}
