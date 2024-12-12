a20c2c216cd372ec9983c61c92f809c17df5036c
BasharMOhamed <bashar.eg6645@gmail.com>
2024-12-12 02:01:27 +0200

    conflict

--- lib/Product.dart
+++ lib/Product.dart
@@ -3,14 +3,9 @@ class Product {
   String name;
   double price;
   String description;
-<<<<<<< HEAD
-<<<<<<< HEAD
- String category;
-int quantityInStock;
-=======
+
   String category;
   int quantityInStock;
->>>>>>> 33b21bb4d0502c9247db7447665b2c03618e1f89
 
   Product(this.imgURL, this.name, this.price, this.description, this.category,
       this.quantityInStock);
@@ -25,8 +20,4 @@ int quantityInStock;
       map['quantityInStock']?.toInt() ?? 0,
     );
   }
-=======
-  String category;
-  Product(this.imgURL, this.name, this.price, this.description, this.category);
->>>>>>> e243eb7 (Finishing the product Details page)
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/productDetails.dart';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 33b21bb4d0502c9247db7447665b2c03618e1f89
 import 'package:flutter_app/firebase_options.dart';
 import 'signUp.dart';
 import 'login.dart';
-=======
->>>>>>> 42a18d6 (Making product details page with dummy data)
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,31 +18,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-<<<<<<< HEAD
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Firebase Sign Up',
       home: LoginPage(),
-=======
-    return const MaterialApp(
-      home: Productdetails(),
-<<<<<<< HEAD
->>>>>>> 4489263 (Making product details page with dummy data)
-=======
-    return MaterialApp(
-      home: Productdetails(
-          productName: "Shaan Body Milk",
-          productPrice: 5,
-          productId: 0,
-          description: "body milk 72 hours 300 ml.",
-          category: "SkinCare",
-          quantityInStock: 1,
-          imageURL:
-              "https://m.media-amazon.com/images/I/51c+8faWUWL._AC_SY300_SX300_.jpg"),
->>>>>>> e243eb7 (Finishing the product Details page)
-=======
->>>>>>> 42a18d6 (Making product details page with dummy data)
     );
   }
 }
